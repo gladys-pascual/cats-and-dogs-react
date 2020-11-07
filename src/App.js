980d86425c6fc3049c11b9e3ee231d87b4f16eaa
gladys-pascual <gladyskate@gmail.com>
2020-11-07 23:27:27 +0000

    fix styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import Dropdown from './Components/Dropdown';
-import AnimalsList from './Components/AnimalsList';
+import React, { Component } from "react";
+import "./App.css";
+import Dropdown from "./Components/Dropdown";
+import AnimalsList from "./Components/AnimalsList";
 
 const cats = [
   {
-    name: 'Kandy',
-    imgUrl: 'https://images-cdn.9gag.com/photo/3630193_700b.jpg',
+    name: "Kandy",
+    imgUrl: "https://images-cdn.9gag.com/photo/3630193_700b.jpg",
   },
   {
-    name: 'Sir John II',
-    imgUrl: 'https://kittybloger.files.wordpress.com/2012/05/cat-king.jpg?w=584',
+    name: "Sir John II",
+    imgUrl:
+      "https://kittybloger.files.wordpress.com/2012/05/cat-king.jpg?w=584",
   },
-]
+];
 
 const dogs = [
   {
-    name: 'Bill',
-    imgUrl: 'https://live.staticflickr.com/8534/8622367912_cfe27bf5ef.jpg',
+    name: "Bill",
+    imgUrl: "https://live.staticflickr.com/8534/8622367912_cfe27bf5ef.jpg",
   },
   {
-    name: 'Shawna',
-    imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk-bi-Tq3VTy8AL_fGBoLIwdnvY5RfG8wJNr1xW5_tWzQ8TOGx',
+    name: "Shawna",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk-bi-Tq3VTy8AL_fGBoLIwdnvY5RfG8wJNr1xW5_tWzQ8TOGx",
   },
 ];
 
@@ -29,30 +31,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selection: 'All'
-    }
+      selection: "All",
+    };
   }
 
   handleAnimalChoice = (event) => {
-    this.setState({ selection: event.target.value })
-  }
+    this.setState({ selection: event.target.value });
+  };
 
   choice = () => {
-    if (this.state.selection === 'Cats') {
-      return cats
-    } else if (this.state.selection === 'Dogs') {
-      return dogs
+    if (this.state.selection === "Cats") {
+      return cats;
+    } else if (this.state.selection === "Dogs") {
+      return dogs;
     } else {
-      return [...cats, ...dogs]
+      return [...cats, ...dogs];
     }
-  }
+  };
 
   render() {
     return (
-      <>
+      <div className="cats-and-dogs">
         <Dropdown handleChoice={this.handleAnimalChoice} />
         <AnimalsList animals={this.choice()} />
-      </>
+      </div>
     );
   }
 }
--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Dropdown = (props) => {
   return (
-    <select onChange={props.handleChoice} name="animals" id="animals">
-      <option value="All">All</option>
-      <option value="Cats">Cats</option>
-      <option value="Dogs">Dogs</option>
-    </select>
-  )
-}
+    <div className="dropdown-wrapper">
+      <select onChange={props.handleChoice} name="animals" id="animals">
+        <option value="All">All</option>
+        <option value="Cats">Cats</option>
+        <option value="Dogs">Dogs</option>
+      </select>
+    </div>
+  );
+};
 
 export default Dropdown;
